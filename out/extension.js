f59ae6929a1f503ad86456d2cf7bdc8654f96b44
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-03-22 23:22:33 +0000

    Delete comments and update command

--- out/extension.js
+++ out/extension.js
@@ -24,8 +24,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(require("vscode"));
 const firstLine = '```mermaid';
 const lastLine = '```';
@@ -38,22 +36,9 @@ const mermaidDefaultGraph = `graph TD
 const mermaidFullContent = `${firstLine}
 ${mermaidDefaultGraph}
 ${lastLine}`;
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "actions-to-graph" is now active!');
-    // // The command has been defined in the package.json file
-    // // Now provide the implementation of the command with registerCommand
-    // // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('actions-to-graph.helloWorld', () => {
-    // 	// The code you place here will be executed every time your command is executed
-    // 	// Display a message box to the user
-    // 	vscode.window.showInformationMessage('Hello World from actions-to-graph!');
-    // });
-    // context.subscriptions.push(disposable);
-    let disposable = vscode.commands.registerCommand('actions-to-graph.helloWorld', async () => {
+    let disposable = vscode.commands.registerCommand('actions-to-graph.generateGraph', async () => {
         const fileUri = vscode.Uri.parse('untitled:graph.md');
         const doc = await vscode.workspace.openTextDocument(fileUri);
         const editor = await vscode.window.showTextDocument(doc);
@@ -65,7 +50,6 @@ function activate(context) {
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// This method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 const firstLine = '```mermaid';
@@ -15,26 +13,10 @@ const mermaidFullContent = `${firstLine}
 ${mermaidDefaultGraph}
 ${lastLine}`;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "actions-to-graph" is now active!');
 
-	// // The command has been defined in the package.json file
-	// // Now provide the implementation of the command with registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('actions-to-graph.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from actions-to-graph!');
-	// });
-
-	// context.subscriptions.push(disposable);
-
-	let disposable = vscode.commands.registerCommand('actions-to-graph.helloWorld', async () => {
+	let disposable = vscode.commands.registerCommand('actions-to-graph.generateGraph', async () => {
     const fileUri = vscode.Uri.parse('untitled:graph.md');
     const doc = await vscode.workspace.openTextDocument(fileUri);
 		const editor = await vscode.window.showTextDocument(doc);
@@ -48,5 +30,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
