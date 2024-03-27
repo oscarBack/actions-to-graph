03820fdd07306071c08d44c920078fceb27b1f86
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-03-27 22:28:12 +0000

    Delete all comments

--- src/gen-mermaid.ts
+++ src/gen-mermaid.ts
@@ -18,7 +18,7 @@ export function generateMermaidSyntax(workflow: Workflow): string {
 
   for (const jobId in workflow.jobs) {
       const job = workflow.jobs[jobId];
-      const jobName = job.name.replace(/\s+/g, '-'); // Replace whitespace with hyphens for Mermaid compatibility
+      const jobName = job.name.replace(/\s+/g, '-');
       mermaidSyntax += `  ${jobName}[\"${job.name}\"]`;
 
       if (Array.isArray(job.needs) && job.needs.length > 0) {
@@ -30,14 +30,11 @@ export function generateMermaidSyntax(workflow: Workflow): string {
               }
           });
       }
-      // if job.needs is not an array and is a string
       else if (typeof job.needs === 'string' && workflow.jobs[job.needs]) {
           const dependencyName = workflow.jobs[job.needs].name.replace(/\s+/g, '-');
           mermaidSyntax += `\n  ${dependencyName}[\"${workflow.jobs[job.needs].name}\"] --> ${jobName}`;
           arreglo.push(mermaidSyntax)
       }
-      // arreglo.push(job)
-      // if this is the last iteration just add a semicolon, otherwise add a semi-colon and a new line
       if (jobId === Object.keys(workflow.jobs)[Object.keys(workflow.jobs).length - 1]) {
           mermaidSyntax += ';';
       } else {
