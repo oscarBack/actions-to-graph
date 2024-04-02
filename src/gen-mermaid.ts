2610f0eb793fddb5eea5bb86c1a5b5b9f6095426
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-04-02 22:44:50 +0000

    Auto fix ts-standard

--- src/gen-mermaid.ts
+++ src/gen-mermaid.ts
@@ -1,50 +1,47 @@
-import fs from 'fs'
 import YAML from 'yaml'
 
 interface Job {
-  name: string;
-  needs?: string[];
+  name: string
+  needs?: string[]
 }
 
 interface Workflow {
-  jobs: { [key: string]: Job };
+  jobs: { [key: string]: Job }
 }
 
-const arreglo : any[] = []
+const arreglo: any[] = []
 
-export function generateMermaidSyntax(workflow: Workflow): string {
-  let mermaidSyntax = 'graph TD;\n';
-
+export function generateMermaidSyntax (workflow: Workflow): string {
+  let mermaidSyntax = 'graph TD;\n'
 
   for (const jobId in workflow.jobs) {
-      const job = workflow.jobs[jobId];
-      const jobName = job.name.replace(/\s+/g, '-');
-      mermaidSyntax += `  ${jobName}[\"${job.name}\"]`;
+    const job = workflow.jobs[jobId]
+    const jobName = job.name.replace(/\s+/g, '-')
+    mermaidSyntax += `  ${jobName}["${job.name}"]`
 
-      if (Array.isArray(job.needs) && job.needs.length > 0) {
-          job.needs.forEach((dependency) => {
-              if (workflow.jobs[dependency]) {
-                  const dependencyName = workflow.jobs[dependency].name.replace(/\s+/g, '-');
-                  mermaidSyntax += `\n  ${dependencyName}[\"${workflow.jobs[dependency].name}\"] --> ${jobName}`;
-                  arreglo.push(mermaidSyntax)
-              }
-          });
-      }
-      else if (typeof job.needs === 'string' && workflow.jobs[job.needs]) {
-          const dependencyName = workflow.jobs[job.needs].name.replace(/\s+/g, '-');
-          mermaidSyntax += `\n  ${dependencyName}[\"${workflow.jobs[job.needs].name}\"] --> ${jobName}`;
+    if (Array.isArray(job.needs) && job.needs.length > 0) {
+      job.needs.forEach((dependency) => {
+        if (workflow.jobs[dependency]) {
+          const dependencyName = workflow.jobs[dependency].name.replace(/\s+/g, '-')
+          mermaidSyntax += `\n  ${dependencyName}["${workflow.jobs[dependency].name}"] --> ${jobName}`
           arreglo.push(mermaidSyntax)
-      }
-      if (jobId === Object.keys(workflow.jobs)[Object.keys(workflow.jobs).length - 1]) {
-          mermaidSyntax += ';';
-      } else {
-          mermaidSyntax += ';\n';
-      }
+        }
+      })
+    } else if (typeof job.needs === 'string' && workflow.jobs[job.needs]) {
+      const dependencyName = workflow.jobs[job.needs].name.replace(/\s+/g, '-')
+      mermaidSyntax += `\n  ${dependencyName}["${workflow.jobs[job.needs].name}"] --> ${jobName}`
+      arreglo.push(mermaidSyntax)
+    }
+    if (jobId === Object.keys(workflow.jobs)[Object.keys(workflow.jobs).length - 1]) {
+      mermaidSyntax += ';'
+    } else {
+      mermaidSyntax += ';\n'
+    }
   }
 
-  return mermaidSyntax;
+  return mermaidSyntax
 }
 
-export function parseYAMLtoJSON(yamlContent: string) {
-  return YAML.parse(yamlContent);
+export function parseYAMLtoJSON (yamlContent: string) {
+  return YAML.parse(yamlContent)
 }
