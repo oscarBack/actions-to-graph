7b90e83aaed60e187a1190e4236be77a1fd4ab35
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-04-13 02:17:27 +0000

    Fix ts standard on extension test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -6,7 +6,9 @@ import * as vscode from 'vscode'
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.')
+  before(async () => {
+    await vscode.window.showInformationMessage('Start all tests.')
+  })
 
   test('Sample test', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5))
