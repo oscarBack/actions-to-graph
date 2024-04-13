fde1fca135e8a3593c762204e60cb516d748fcd2
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-04-13 01:31:16 +0000

    Fix of shared libraries

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,15 @@ sudo chown node /workspace/node_modules
 npm install -g yo generator-code
 npm install -g ts-standard
 npm i
+
+# Install shared libraries
+# update
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install libasound2 -y
+sudo apt-get -y install libnss3
+sudo aptitude -y install libatk1.0-0
+sudo apt -y install libasound2-plugins-extra
+sudo apt-get -y install libgtk-3-dev
+sudo apt-get -y install libdrm-dev
+sudo apt-get -y install libatk-bridge2.0-0
+sudo apt-get -y install libdbus-1-3
