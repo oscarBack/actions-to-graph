3cc94067ed536778ed8aaecc8ebd4c924c162610
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-04-13 02:12:00 +0000

    reorder installations

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,9 @@
+# Install shared libraries
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y libasound2 libnss3 libatk1.0-0 libgtk-3-dev libdrm-dev libatk-bridge2.0-0 libdbus-1-3
+
 sudo chown node /workspace/node_modules
 npm install -g yo generator-code
 npm install -g ts-standard
 npm i
 
-# Install shared libraries
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y libasound2 libnss3 libatk1.0-0 libgtk-3-dev libdrm-dev libatk-bridge2.0-0 libdbus-1-3
