c8b4c13345c8c821ed3fc3d71eccbd34b7ffe586
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-04-19 01:24:21 +0000

    Add first approach to test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,71 +1,43 @@
 import * as assert from 'assert'
+import { before } from 'node:test'
+import * as fs from 'fs'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode'
 // import * as myExtension from '../../extension';
 
+const pathYamlBasic1: string = 'src/test/pool_examples/basic_1.yml'
+const pathMdBasic1: string = 'src/test/pool_examples/basic_1.md'
+
+const yamlBasic1 = fs.readFileSync(pathYamlBasic1, 'utf8')
+const mdBasic1 = fs.readFileSync(pathMdBasic1, 'utf8')
+
 suite('Extension Test Suite', () => {
-  // before(async () => {
-  //   await vscode.window.showInformationMessage('Start all tests.')
-  // })
-
-  test('Sample test', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5))
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0))
+  before(async () => {
+    await vscode.window.showInformationMessage('Start all tests.')
+    const extension = vscode.extensions.getExtension('oscarBack.actions-to-graph')
+    if (extension === null || extension === undefined) {
+      throw new Error('Extension not found')
+    }
+    await extension.activate()
   })
-})
-
-// Set a new test suite that open the editor and fill the content with a sample YAML
-suite('Generate Graph Test Suite', () => {
-  test('Generate Graph', async () => {
-    const ymlContent = `
-      name: Test
-      on:
-        push:
-          branches:
-            - master
-      jobs:
-        build:
-          runs-on: ubuntu-latest
-          steps:
-            - uses: actions/checkout@v2
-            - name: Run a one-line script
-              run: echo Hello, world!
-        tag:
-          needs: build
-          runs-on: ubuntu-latest
-          steps:
-            - name: Tag the commit
-              run: echo "Tagging the commit"
-        deploy:
-          needs: tag
-          runs-on: ubuntu-latest
-          steps:
-            - name: Deploy to production
-              run: echo "Deploying to production"
-    `
-    const mdResultFirstLine = '```mermaid'
-
-    const doc = await vscode.workspace.openTextDocument({
-      content: ymlContent,
+  test('Sample test', async () => {
+    const yamlEditor = await vscode.workspace.openTextDocument({
+      content: yamlBasic1,
       language: 'yaml'
     })
-    const editor = await vscode.window.showTextDocument(doc)
-    // execute the command
+    // get the content of the yamlEditor
+    const yamlEditorContent = yamlEditor.getText()
+    // console.log('yamlEditorContent', yamlEditorContent)
+    await vscode.window.showTextDocument(yamlEditor)
     await vscode.commands.executeCommand('actions-to-graph.generateGraph')
-    // get the content of the editor
-    // const editorContent = editor.document.getText()
-
-    // Get the content of the edito on the new file that was created
-    const editorContent = vscode.window.activeTextEditor?.document.getText() ?? ''
-    console.log(editorContent)
-    // check if the first line is ```mermaid
-    assert.strictEqual(editorContent.split('\n')[0], mdResultFirstLine)
-
-    // compare if true is equal to true
-    assert.strictEqual(true, true)
-  }
-  )
-}
-)
+    // get the content of the new document opened
+    const newEditor = vscode.window.activeTextEditor
+    const newEditorContent = newEditor?.document.getText()
+    console.log('newEditorContent', newEditorContent)
+    // assert.strictEqual(newEditorContent, mdBasic1)
+    assert.strictEqual([1, 2, 3].indexOf(5), -1)
+    assert.strictEqual([1, 2, 3].indexOf(0), -1)
+  })
+})
---
+++ src/test/pool_examples/basic_1.md
@@ -0,0 +1,9 @@
+```mermaid
+graph TD;
+  build["build"];
+  tag["tag"]
+  build["build"] --> tag;
+  deploy["deploy"]
+  tag["tag"] --> deploy;
+```
+asasa
