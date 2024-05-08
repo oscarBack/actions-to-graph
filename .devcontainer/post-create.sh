349519a1c2ddfc5349dac4de4c63a4cf7cc31195
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-05-08 01:53:52 +0000

    feat: add volume for node_modules

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y libasound2 libnss3 libatk1.0-0 libgtk-3-dev libdrm-dev libatk-bridge2.0-0 libdbus-1-3
 
-# sudo chown node /workspaces/node_modules
+sudo chown node /workspaces/actions-to-graph/node_modules
 npm install -g yo generator-code ts-standard
 npm ci
 
