7b4446d073ad23ca2130bb06d445c911e2b1ff70
Oscar Acevedo <oscar.jair.it@gmail.com>
2024-05-08 00:46:49 +0000

    feat: add github cli inside devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,8 +2,8 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y libasound2 libnss3 libatk1.0-0 libgtk-3-dev libdrm-dev libatk-bridge2.0-0 libdbus-1-3
 
-sudo chown node /workspace/node_modules
-npm install -g yo generator-code
-npm install -g ts-standard
-npm i
+# sudo chown node /workspaces/node_modules
+npm install -g yo generator-code ts-standard
+npm ci
 
+gh auth login --with-token < .devcontainer/token.txt
